5d41bfd71a12378d5b7dd7ba979323c34ef74cba
Mbaye <mmbaye1@hotmail.fr>
2021-12-29 09:32:00 +0100

    params emailjs ajoutés

--- src/App.js
+++ src/App.js
@@ -1,11 +1,104 @@
+import React, { useState } from "react";
 import "./App.css";
+import emailjs from "emailjs-com";
+import { init } from "emailjs-com";
+init("user_feVY2TIc0JlRyKZ7ySnLq");
 
-function App() {
+const App = () => {
+  const [name, setName] = useState("");
+  const [company, setCompany] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    sendFeedback("template_batm9jj", {
+      name,
+      company,
+      phone,
+      email,
+      message,
+    });
+  };
+
+  const sendFeedback = (templateId, variables) => {
+    emailjs
+      .send("service_7yfo8bc", templateId, variables)
+      .then((res) => {
+        console.log("success !");
+        setName("");
+        setCompany("");
+        setPhone("");
+        setEmail("");
+        setMessage("");
+      })
+      .catch(
+        (err) =>
+          (document.querySelector(".form-message").innerHTML =
+            "Une erreur s'est produite, veuillez réessayer.")
+      );
+  };
+
   return (
-    <div className="App">
-      <h1>Hello react</h1>
-    </div>
+    <form className="contact-form">
+      <h2>Contactez-nous</h2>
+      <div className="form-content">
+        <input
+          type="text"
+          id="name"
+          name="name"
+          onChange={(e) => setName(e.target.value)}
+          placeholder="nom *"
+          value={name}
+          autoComplete="off"
+        />
+        <input
+          type="text"
+          id="company"
+          name="company"
+          onChange={(e) => setCompany(e.target.value)}
+          placeholder="société"
+          value={company}
+        />
+        <input
+          type="text"
+          id="phone"
+          name="phone"
+          onChange={(e) => setPhone(e.target.value)}
+          placeholder="téléphone"
+          value={phone}
+        />
+        <div className="email-content">
+          <label id="not-mail">Email non valide</label>
+          <input
+            type="mail"
+            id="email"
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="email *"
+            value={email}
+            autoComplete="off"
+          />
+        </div>
+        <textarea
+          id="message"
+          name="message"
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="message *"
+          value={message}
+        />
+      </div>
+      <input
+        className="button"
+        type="button"
+        value="Envoyer"
+        onClick={handleSubmit}
+      />
+      <div className="form-message"></div>
+    </form>
   );
-}
+};
 
 export default App;
